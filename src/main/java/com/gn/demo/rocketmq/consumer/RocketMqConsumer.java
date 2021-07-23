8f95d77ea9ea8e2f4f6ed0c14eb70985fe8234b9
g493562429 <493562429@qq.com>
2021-07-23 14:30:34 +0800

    20210723 增加rocketmqDemo

--- src/main/java/com/gn/demo/rocketmq/consumer/RocketMqConsumer.java
+++ src/main/java/com/gn/demo/rocketmq/consumer/RocketMqConsumer.java
@@ -18,7 +18,7 @@ import javax.annotation.PostConstruct;
 @Component
 @Slf4j
 public class RocketMqConsumer {
-    @Value("${gn.ceshi.mq.nameSvrAddr:127.0.0.1:9876}")
+    @Value("${gn.ceshi.mq.rocketmq.nameSvrAddr}")
     private String nameSvrAddr;
 
     @Value("${gn.ceshi.mq.topic}")
